2143f94456335ea82947fbe0bb9ab93c1be0c1b9
Rodrigo Moyle <rodrigorm@gmail.com>
2025-02-06 13:05:43 +0000

    Add support to debian container

--- home/.bashrc.d/10-path.sh
+++ home/.bashrc.d/10-path.sh
@@ -4,7 +4,7 @@
 [ -d "$HOME/bin" ] && PATH="$HOME/bin:$PATH"
 
 # Add RVM to PATH for scripting
-[ -d "$HOME/.rvm/bin" ] && PATH="$HOME/.rvm/bin:$PATH" 
+[ -d "$HOME/.rvm/bin" ] && PATH="$HOME/.rvm/bin:$PATH"
 
 [ -d "/usr/local/sbin" ] && PATH="/usr/local/sbin:$PATH"
 
@@ -12,12 +12,12 @@
 
 [ -d "$HOME/.composer/vendor/bin" ] && PATH="$HOME/.composer/vendor/bin:$PATH"
 
-if [ -d "$HOME/Library/Android/sdk" ] ; then
-  export ANDROID_HOME="$HOME/Library/Android/sdk"
-  export PATH="$ANDROID_HOME/emulator:$PATH"
-  export PATH="$ANDROID_HOME/tools:$PATH"
-  export PATH="$ANDROID_HOME/tools/bin:$PATH"
-  export PATH="$ANDROID_HOME/platform-tools:$PATH"
+if [ -d "$HOME/Library/Android/sdk" ]; then
+    export ANDROID_HOME="$HOME/Library/Android/sdk"
+    export PATH="$ANDROID_HOME/emulator:$PATH"
+    export PATH="$ANDROID_HOME/tools:$PATH"
+    export PATH="$ANDROID_HOME/tools/bin:$PATH"
+    export PATH="$ANDROID_HOME/platform-tools:$PATH"
 fi
 
 [ -d "$HOME/.virtualenv/bin" ] && PATH="$HOME/.virtualenv/bin:$PATH"
@@ -25,3 +25,5 @@ fi
 [ -d "/usr/local/go/bin" ] && PATH="/usr/local/go/bin:$PATH"
 
 [ -d "$HOME/.cargo/bin" ] && PATH="$HOME/.cargo/bin:$PATH"
+
+[ -d "/opt/nvim/bin" ] && PATH="/opt/nvim/bin:$PATH"
