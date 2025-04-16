9e10652fac64ce449f87a3ff3205b5ead6f6ef69
Rodrigo Moyle <rodrigorm@gmail.com>
2025-04-16 09:16:34 -0300

    Fix setup on macos

--- home/.bashrc.d/10-homebrew.sh
+++ home/.bashrc.d/10-homebrew.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Setup shell for Homebrew
-if command -v brew >/dev/null 2>&1; then
-    eval "$(brew shellenv)"
+if command -v /opt/homebrew/bin/brew >/dev/null 2>&1; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 
     if [ -f "$HOMEBREW_PREFIX/etc/profile.d/bash_completion.sh" ]; then
         # shellcheck source=/dev/null
