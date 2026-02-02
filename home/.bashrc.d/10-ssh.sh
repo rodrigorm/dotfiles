c5e896a326f18b1d3c93f86c0c265c6cd84d6fd8
Rodrigo Moyle <rodrigorm@gmail.com>
2026-02-02 15:41:17 -0300

    Auto-load SSH keys on all sessions (not just local)

--- home/.bashrc.d/10-ssh.sh
+++ home/.bashrc.d/10-ssh.sh
@@ -4,24 +4,14 @@
 # setup ssh-agent
 #
 
-# start agent if necessary
-if [ -z "$SSH_AUTH_SOCK" ] && [ -z "$SSH_TTY" ]; then # if no agent & not in ssh
+# Start agent and add keys if not already running
+if [ -z "$SSH_AUTH_SOCK" ]; then
     eval "$(ssh-agent -s)" >/dev/null
+    ssh-add 2>/dev/null || true
 fi
 
 # redirect $SSH_AUTH_SOCK from connection to keep screen sessions connected with agent
 if [[ -S "$SSH_AUTH_SOCK" && ! -L "$SSH_AUTH_SOCK" ]]; then
     ln -sf "$SSH_AUTH_SOCK" "$HOME/.ssh/ssh_auth_sock"
     export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
-fi
-
-# setup addition of keys when needed
-if [ -z "$SSH_TTY" ]; then           # if not using ssh
-    if ! ssh-add -l >/dev/null; then # check for keys
-        alias ssh='ssh-add -l > /dev/null || ssh-add && unalias ssh ; ssh'
-        if [ -f "/usr/lib/ssh/x11-ssh-askpass" ]; then
-            SSH_ASKPASS="/usr/lib/ssh/x11-ssh-askpass"
-            export SSH_ASKPASS
-        fi
-    fi
 fi
